f7e3fe86143642a4a36e6a5819c5eea6ae9eba0e
junichi4250 <jkky0225@gmail.com>
2021-12-23 19:39:12 +0900

    refs:request handler

--- api/main.go
+++ api/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"time"
 
-func withApiKey(fn http.HandlerFunc) http.HandlerFunc {
+	"github.com/stretchr/graceful"
+
+	"gopkg.in/mgo.v2"
+)
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if !isValidAPIKey(r.URL.Query().Get("key")) {
 			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
@@ -27,11 +36,34 @@ func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
 
 func withVars(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		OpenVars()
-		defer CloseVars()
+		OpenVars(r)
+		defer CloseVars(r)
+		fn(w, r)
+	}
+}
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
 		fn(w, r)
 	}
 }
 func main() {
-
+	var (
+		addr  = flag.String("addr", ":8080", "エンドポイントのアドレス")
+		mongo = flag.String("mongo", "localhost", "MongoDBのアドレス")
+	)
+	flag.Parse()
+	log.Println("MongoDBに接続します", *mongo)
+	db, err := mgo.Dial(*mongo)
+	if err != nil {
+		log.Fatalln("MongoDBへの接続に失敗しました:", err)
+	}
+	defer db.Close()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/polls/", withCORS(withVars(withData(db,
+		withAPIKey(handlePolls)))))
+	log.Println("Webサーバーを開始します:", *addr)
+	graceful.Run(*addr, 1*time.Second, mux)
+	log.Println("停止します...")
 }
